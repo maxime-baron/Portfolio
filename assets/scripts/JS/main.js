b97c54ab8a735e1d0dc20d9caa7d5c75bfc48401
Ewixam <74783098+Ewixam@users.noreply.github.com>
2022-06-17 19:04:06 +0200

    🚀 Deployment

---
+++ assets/scripts/JS/main.js
@@ -0,0 +1,183 @@
+const sections = [...document.querySelectorAll("Section")]
+const menuItem = [...document.querySelectorAll("nav a")]
+let sectionsPosition;
+
+positionCalculation()
+menuSwitch()
+pageNumSwitch()
+pageNumAdd()
+
+menuItem.forEach(link => link.addEventListener("click", smoothScroll))
+
+document.addEventListener('scroll', () => {
+    phraseSwitch()
+    menuSwitch()
+    pageNumSwitch()
+    pageNumAdd()
+    textAppearance()
+})
+
+document.querySelector(".burgerMenu").addEventListener("click", () => {
+    if (!document.querySelector("#menu").classList.contains("open")) {
+        openMenu()
+    }
+    else if (document.querySelector("#menu").classList.contains("open")) {
+        closeMenu()
+    }
+})
+
+//Menu effet gris
+document.querySelectorAll(".menuLabel").forEach((label) => {
+    label.addEventListener("mouseover", () => {
+        document.querySelectorAll(".menuLabel").forEach((labl) => {
+            if (labl != label) {
+                labl.style.color = "#8D8D8D"
+            }
+        })
+    })
+    label.addEventListener("mouseout", () => {
+        document.querySelectorAll(".menuLabel").forEach((labl) => {
+            if (labl.style.color != "#8D8D8D") {
+                labl.style.color = "#FAFAFA"
+            }
+        })
+    })
+})
+
+// Changement de couleur de la phrase
+function phraseSwitch() {
+    let about = document.querySelector('#aboutMe')
+    let alt = document.querySelector(".alt")
+    let start = document.querySelector(".start")
+
+    if (document.querySelector("#home").getBoundingClientRect().bottom < window.innerHeight)
+        start.style.display = "none"
+    else
+        start.style.display = "block"
+
+    // if (parseInt(alt.style.top) < 0)
+    //     start.style.visibility = "visible"
+    // else
+    //     start.style.visibility = "hidden"
+
+    if (parseInt(start.offsetTop) > about.getBoundingClientRect().top)
+        start.style.position = "relative"
+    else
+        start.style.position = "fixed"
+
+    alt.style.top = start.offsetTop - alt.closest(".sec").getBoundingClientRect().top + "px"
+    alt.style.opacity = Math.pow(about.getBoundingClientRect().top / window.innerHeight, 6)
+}
+
+//Ouverture du menu
+function openMenu() {
+    document.querySelector("#menu").classList.add("open")
+    document.querySelector(".burgerMenu").classList.add("open")
+    document.querySelector(".pageInfo").classList.add("open")
+    disableScroll()
+    phraseSwitch()
+    menuSwitch()
+}
+
+//Fermeture du menu
+function closeMenu() {
+    document.querySelector("#menu").classList.remove("open")
+    document.querySelector(".burgerMenu").classList.remove("open")
+    document.querySelector(".pageInfo").classList.remove("open")
+    enableScroll()
+    phraseSwitch()
+    menuSwitch()
+}
+//Smooth scroll direction
+function smoothScroll(e) {
+    document.querySelector("#menu").classList.remove("open")
+    document.querySelector(".burgerMenu").classList.remove("open")
+    document.querySelector(".pageInfo").classList.remove("open")
+    enableScroll()
+    phraseSwitch()
+    menuSwitch()
+    const linkIndex = menuItem.indexOf(e.target.parentElement)
+    window.scrollTo({
+        top: sectionsPosition[linkIndex],
+        behavior: "smooth"
+    })
+}
+
+//Apparition des textes
+function textAppearance() {
+    let h3 = document.querySelectorAll("h3")
+    h3.forEach((elmt) => {
+        if (elmt.getBoundingClientRect().top < window.innerHeight) {
+            elmt.firstChild.classList.add("ap")
+            elmt.classList.add("ap")
+        }
+    })
+}
+
+//récupéré les position des secitons
+function positionCalculation() {
+    sectionsPosition = sections.map(section => section.offsetTop)
+}
+
+//Décompte des pages
+function pageNumAdd() {
+    let pageInfo = document.querySelector(".pageInfo")
+    let home = document.querySelector("#home")
+    let aboutMe = document.querySelector("#aboutMe")
+    let portfolio = document.querySelector("#portfolio")
+    let skills = document.querySelector("#skills")
+
+    if (home.getBoundingClientRect().top < pageInfo.getBoundingClientRect().top && home.getBoundingClientRect().bottom > pageInfo.getBoundingClientRect().bottom) {
+        pageInfo.children[0].innerText = "01"
+    }
+
+    if (aboutMe.getBoundingClientRect().top < pageInfo.getBoundingClientRect().top && aboutMe.getBoundingClientRect().bottom > pageInfo.getBoundingClientRect().bottom) {
+        pageInfo.children[0].innerText = "02"
+    }
+
+    if (portfolio.getBoundingClientRect().top < pageInfo.getBoundingClientRect().top && portfolio.getBoundingClientRect().bottom > pageInfo.getBoundingClientRect().bottom) {
+        pageInfo.children[0].innerText = "03"
+    }
+
+    if (skills.getBoundingClientRect().top < pageInfo.getBoundingClientRect().top && skills.getBoundingClientRect().bottom > pageInfo.getBoundingClientRect().bottom) {
+        pageInfo.children[0].innerText = "04"
+    }
+}
+
+//Changement de couleur en fonction du font
+function pageNumSwitch() {
+    let WhiteSec = document.querySelectorAll('.whiteBack')
+    let pageInfo = document.querySelector(".pageInfo")
+    let isWhite = false
+    WhiteSec.forEach((white) => {
+        if (white.getBoundingClientRect().top < pageInfo.getBoundingClientRect().top && white.getBoundingClientRect().bottom > pageInfo.getBoundingClientRect().bottom) {
+            isWhite = true
+        }
+    })
+    for (let i = 0; i < pageInfo.childElementCount; i++) {
+        pageInfo.children.item(i).style.transition = i + "s"
+        if (pageInfo.children.item(i).classList.contains("line")) {
+            isWhite ? pageInfo.children.item(i).style.backgroundColor = "#101010" : pageInfo.children.item(i).style.backgroundColor = "#FAFAFA"
+        }
+        else {
+            isWhite ? pageInfo.children.item(i).style.color = "#101010" : pageInfo.children.item(i).style.color = "#FAFAFA"
+        }
+    }
+}
+
+//Changement de couleur en fonction du font
+function menuSwitch() {
+    let WhiteSec = document.querySelectorAll('.whiteBack')
+    let menu = document.querySelector(".burgerMenu")
+    let isWhite = false
+    WhiteSec.forEach((white) => {
+        if (white.getBoundingClientRect().top < menu.getBoundingClientRect().top && white.getBoundingClientRect().bottom > menu.getBoundingClientRect().bottom) {
+            isWhite = true
+        }
+    })
+
+    isWhite ? menu.classList.add('black') : menu.classList.remove('black')
+
+    if (menu.classList.contains("open"))
+        menu.classList.remove('black')
+}
